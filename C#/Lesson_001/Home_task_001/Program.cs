5205939871c0ac31509bd233823dbb112410403c
Nick <srac.ivanov@yandex.ru>
2022-08-19 20:18:21 +0300

    change scheme of algorithm

--- C#/Lesson_001/Home_task_001/Program.cs
+++ C#/Lesson_001/Home_task_001/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number_a = 5;
+int number_b = 7;
+int max = Console.ReadLine();
+if (number_a < number_b)
+{
+    Console.WriteLine(max = number_b);
+}
+else
+{
+    Console.WriteLine ("Ошибка");
+}
+Console.Write(max);
+
