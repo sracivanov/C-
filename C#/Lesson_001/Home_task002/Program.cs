d6a6dc53193a73a14375ac19dad572e5e738961f
Nick <srac.ivanov@yandex.ru>
2022-08-19 23:24:40 +0300

    Add code

---
+++ C#/Lesson_001/Home_task002/Program.cs
@@ -0,0 +1,9 @@
+﻿int number_a = 22;
+int number_b = 3;
+int number_c = 9;
+int max = number_a;
+if (number_a > max) max = number_a;
+if (number_b > max) max = number_b;
+if (number_c > max) max = number_c;
+Console.Write("max=");
+Console.WriteLine(max);
