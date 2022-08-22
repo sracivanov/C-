2b02a4f5bcc53a9571646b7e2f83fd6eec6d296c
Nick <srac.ivanov@yandex.ru>
2022-08-22 20:08:16 +0300

    Add Code Task_03

--- Home_Task_03/Program.cs
+++ Home_Task_03/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿int a = 4;
+int b = -3;
+int c = 7;
+if (a%2 ==0)
+{
+    Console.WriteLine ("Да");
+}
+if (b%2 ==0)
+{
+    Console.WriteLine ("Да");
+}
+if (c%2 ==0)
+{
+    Console.WriteLine ("Да");
+}
