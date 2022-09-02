705515a5a85198b76fcec718ece893644d58d50b
Nick <srac.ivanov@yandex.ru>
2022-09-02 22:10:02 +0300

    add program.cs

---
+++ Lesson_002_task_3/Program.cs
@@ -0,0 +1,10 @@
+﻿int value = new Random(). Next (1, 7);
+Console.WriteLine(value);
+if (value > 5)
+{
+    Console.WriteLine ("Да, это выходной");
+}
+else
+{
+       Console.WriteLine ("нет, это не выходной");
+}
