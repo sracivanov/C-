cb25020d60a2bf6a17ccb92e27f987dabb4c2343
Nick <srac.ivanov@yandex.ru>
2022-09-06 18:40:24 +0300

    add solve change simbols

---
+++ Lecture_003_Task_change_simbols/Program.cs
@@ -0,0 +1,30 @@
+﻿// "Рядом с домом росли высокие деревья и располагалась лавочка"
+
+// string s = "qwerty"
+//             012345  
+// s / 5            y 
+
+string text = "hello world";
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+
+    int lenght = text.Length;
+    for (int i = 0; i < lenght; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+        return result;
+}
+
+string newText = Replace(text, 'h', 'H');
+Console.WriteLine(newText);
+
+newText = Replace(newText, 'w', 'W');
+Console.WriteLine(newText);
+
+newText = Replace(newText, ' ', '|');
+Console.WriteLine(newText);
+
+
